96f22c66da8c7412cca15753bd63181cd7a7eeba
jansony1 <ncatyu@126.com>
2019-07-10 00:47:52 +0000

    configure cognito

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 window._config = {
     cognito: {
-        userPoolId: '-west-2_Lrfhijdxo, // e.g. us-east-2_uXboG5pAb
+        userPoolId: 'us-west-2_Lrfhijdxo', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '1u1vb6rl1gtfvqtutp3gphtr0s', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-west-2' // e.g. us-east-2
     },
